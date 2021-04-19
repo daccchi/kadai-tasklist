b5f8210944ee91eba5b732151587ebca5b790c0f
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-19 14:11:14 +0000

    forth

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,10 +16,10 @@ class TasksController < ApplicationController
       @message = Message.new(message_params)
 
     if @message.save
-      flash[:success] = 'Message が正常に投稿されました'
+      flash[:success] = 'Task が正常に投稿されました'
       redirect_to @message
     else
-      flash.now[:danger] = 'Message が投稿されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
@@ -32,10 +32,10 @@ class TasksController < ApplicationController
       @message = Message.find(params[:id])
 
     if @message.update(message_params)
-      flash[:success] = 'Message は正常に更新されました'
+      flash[:success] = 'Task は正常に更新されました'
       redirect_to @message
     else
-      flash.now[:danger] = 'Message は更新されませんでした'
+      flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
       @message = Message.find(params[:id])
     @message.destroy
 
-    flash[:success] = 'Message は正常に削除されました'
+    flash[:success] = 'Task は正常に削除されました'
     redirect_to messages_url
   end
   
