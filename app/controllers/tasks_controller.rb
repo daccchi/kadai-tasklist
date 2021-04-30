a9e2f9afa7075c58285070af4914a1c4d6ae4a0e
taro.kirameki <taro.kirameki@techacademy.jp>
2021-04-30 07:55:04 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
+  
+   before_action :require_user_logged_in
+   before_action :correct_user, only: [:destroy,:edit,:update,:show]
     
   def index
       @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
+     
   end
 
   def new
@@ -13,7 +16,10 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      
+      @task = current_user.tasks.build(task_params)
+      
+      #@task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -25,11 +31,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+   
   end
 
   def update
-      @task = Task.find(params[:id])
+      
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +47,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
+     
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -58,5 +64,13 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content,:status)
   end
+    
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+    
     
 end
